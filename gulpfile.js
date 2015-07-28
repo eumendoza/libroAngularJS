b66864e6fb468b3501bd3fb79bfcb24c7219982f
Eugenio Mendoza <eu.mendoza@gmail.com>
2015-07-28 14:05:41 +0200

    Actualización del gulpfile con el task del jshint y del package

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
+'use strict';
+
 var gulp = require('gulp'),
-connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    stylus = require('gulp-stylus'),
+    nib = require('nib'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish');
 
 gulp.task('server', function() {
   connect.server({
@@ -10,24 +16,29 @@ gulp.task('server', function() {
   });
 });
 
-var stylus = require('gulp-stylus'),
-nib = require('nib');
+gulp.task('jshint', function(){
+  return gulp.src('./app/scripts/**/*.js')
+  .pipe(jshint('.jshintrc'))
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(jshint.reporter('fail'));
+});
 
 gulp.task('css', function() {
-gulp.src('./app/stylesheets/main.styl')
-.pipe(stylus({ use: nib() }))
-.pipe(gulp.dest('./app/stylesheets'))
-.pipe(connect.reload());
+  gulp.src('./app/stylesheets/main.styl')
+  .pipe(stylus({ use: nib() }))
+  .pipe(gulp.dest('./app/stylesheets'))
+  .pipe(connect.reload());
 });
 
 gulp.task('html', function() {
-gulp.src('./app/**/*.html')
-.pipe(connect.reload());
+  gulp.src('./app/**/*.html')
+  .pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
-gulp.watch(['./app/**/*.html'], ['html']);
-gulp.watch(['./app/stylesheets/**/*.styl'], ['css']);
+  gulp.watch(['./app/**/*.html'], ['html']);
+  gulp.watch(['./app/stylesheets/**/*.styl'], ['css']);
+  gulp.watch(['./app/**/*.js'], ['jshint']);
 });
 
 gulp.task('default', ['server', 'watch']);
